b2c604201c79300dc1598f8cb6171e1de99d389a
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-06-11 12:40:53 +1000

    comments on R-implemented S2S link function

--- R/meanlinkS2S.R
+++ R/meanlinkS2S.R
@@ -16,11 +16,13 @@ iSp=function(y) 1/(1+vnorm(y)^2)*c(1-vnorm(y)^2,2*y[1:(d-1)])
 
 #' The mean link for spherical covariates
 #' @param x a vector of covariate values
-#' @param P P matrix
+#' @param P P matrix: a p x p (?orthonormal) matrix
 #' @param B B matrix: a (p-1) x (p-1) diagonal matrix with elements between zero and one ordered in decreasing size.
-#' @param Q The rotation matrix `Q` for rotating the covariate vector `x`.
+#' @param Q The rotation-like matrix `Q` for rotating the covariate vector `x`.
 #' @export
 meanlinkS2S <- function(x,P,Q,B) P%*%iSp(B%*%Sp(t(Q)%*%x))
 
 mu <- meanlinkS2S
 
+# want meanlinkS2S with more indenpendent paramaters: p1, q1, Omega
+# would be nice to have the diagonal elements separate too?

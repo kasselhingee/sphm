4c968d062ae9885dc937d5d4dba16333e223f966
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-07-11 12:52:02 +1000

    meanlinkS2S.cpp compiles!

--- src/meanlinkS2S.cpp
+++ src/meanlinkS2S.cpp
@@ -4,32 +4,41 @@ using namespace Eigen;
 
 // [[Rcpp::depends(RcppEigen)]]
 
+// @param x is made of column vectors of covariates
+// @param vec is the vectorised form of an OmegaS2S parameterisation
+// @param p is the dimension of the response (in ambient space), which is needed to separate `vec` into p1, q1 and Omega.
+// @value is a matrix of column vectors of means
 // [[Rcpp::export]]
-Eigen::MatrixXd meanlinkS2Scpp(const Eigen::VectorXd &x, const NumericVector &vec, int p) {
+Eigen::MatrixXd meanlinkS2Scpp(const Eigen::VectorXd &x, const Eigen::VectorXd &vec, int p) {
   int n = vec.size();
   int q = (n - p) / (1 + p);
 
-  if (q != (int)q) {
-    stop("q is not an integer");
+  if (q != static_cast<int>(q)) {
+    Rcpp::stop("q is not an integer");
   }
 
   if (q <= p - 1) {
-    stop("q must be greater than p - 1");
+    Rcpp::stop("q must be greater than p - 1");
   }
 
   // Extract p1, q1, and Omega
-  Eigen::VectorXd p1 = Eigen::Map<Eigen::VectorXd>(vec.begin(), p);
-  Eigen::VectorXd q1 = Eigen::Map<Eigen::VectorXd>(vec.begin() + p, q);
-  Eigen::MatrixXd Omega = Eigen::Map<Eigen::MatrixXd>(vec.begin() + p + q, p, q);
+  Eigen::VectorXd p1 = vec.segment(0, p);
+  Eigen::VectorXd q1 = vec.segment(p, q);
+  Eigen::MatrixXd Omega = Eigen::Map<const Eigen::MatrixXd>(vec.segment(p + q, p * q).data(), p, q);
 
-  // Perform matrix calculations
+  // Compute the amount of p1 and Omegax corresponding to each column of x
   Eigen::VectorXd BQx2 = (Omega.transpose() * Omega * x).cwiseProduct(x).colwise().sum();
-  double q1x = q1.dot(x);
-  Eigen::VectorXd p1_mult = ((1 + q1x) * (1 + q1x) - BQx2).array() / ((1 + q1x) * (1 + q1x) + BQx2).array();
-  Eigen::VectorXd Omegax_mult = (2 * (1 + q1x)) / ((1 + q1x) * (1 + q1x) + BQx2).array();
+  Eigen::VectorXd q1x = q1.transpose() * x;
+  Eigen::VectorXd p1_mult = ((q1x.array() + 1).square() - BQx2.array()) / ((q1x.array() + 1).square() + BQx2.array());
+  Eigen::VectorXd Omegax_mult = (2 * (1 + q1x.array())) / ((q1x.array() + 1).square() + BQx2.array());
 
-  // Compute the outer product
-  Eigen::MatrixXd out = p1_mult * p1.transpose() + Omegax_mult.asDiagonal() * Omega * x;
+  // For each column of x combine p1 and Omega * x according to the above amounts. 
+  Eigen::MatrixXd p1_part = (p1_mult * p1.transpose()).transpose(); //each column should be p1 scaled by the entry of p1_mult
+  Eigen::MatrixXd Omegax_part = Omega * x;
+  for (int col = 0; col < Omegax_part.cols(); ++col){
+    Omegax_part.col(col) = Omegax_part.col(col) * Omegax_mult(col);
+  }
+  Eigen::MatrixXd out = p1_part + Omegax_part;
 
   return out;
 }

e76488bf59a7e3bc9664ce405998d8f35a1a3263
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-08-20 15:52:25 +1000

    two SvMF parameterisations implemented

--- R/SvMF.R
+++ R/SvMF.R
@@ -1,12 +1,19 @@
 #' SvMF Parameterisations
+#' @rdname SvMFparams
 #' The SvMF canonical parameterisation is a set of positive real numbers (kappa, `a`) and a matrix of orthonormal column vectors `Gamma`, where the product of the `a` is 1. `a_1` and `kappa` are not jointly estimatable by MLE, so I'm expecting them to be closely related.
 #' There are three other parametrisations.
 #' Alternatively this set of numbers are the eigenvalues and eigenvectors of the symmetric matrix E(yy^T). The determinant of this matrix would be kappa * a_1.
-#' A third parametrisation is available: using kappa, mu and a matrix V that is symmetric positive definite with det(V) = 1.
+#' A third parametrisation is available: using kappa, mu and a matrix V that is symmetric positive definite matrix with det(V) = 1.
 #' A fourth represents the above V matrix as an orthogonal matrix and a set of positive real values.
+#' 
+#' @examples 
+#' SvMFcann(kappa = 0.5, a = c(2, rep(1, 5-1)), Gamma = diag(1, 5))
 
-SvMFcann <- function(kappa, a, Gamma){
-  obj <- list(k = kappa, a = a, G = Gamma)
+NULL
+
+#' @describeIn SvMFparams The canonical parameterisation of a SvMF using a kappa, a vector and Gamma, as used in equation 7 of Scealy and Wood 2017.
+SvMFcann <- function(k, a, G){
+  obj <- list(k = k, a = a, G = G)
   class(obj) <- c("SvMFcann", class(obj))
   return(obj)
 }
@@ -17,13 +24,56 @@ SvMFcann_check <- function(obj){
   `Gamma square` = ncol(G) == nrow(G),
   `kappa positive` = k > 0,
   `a positive` = a > 0,
-  `a product` = (prod(a[-1]) - 1)^2 < sqrt(.Matchine$double.eps),
-  `Gamma orthogonal` = (t(G) %*% G - diag(1, ncol(G)))^2 <  sqrt(.Matchine$double.eps)
+  `a product` = (prod(a[-1]) - 1)^2 < sqrt(.Machine$double.eps),
+  `Gamma orthogonal` = (t(G) %*% G - diag(1, ncol(G)))^2 <  sqrt(.Machine$double.eps)
   )
   if (any(!checks)){
-    stop(sprinf("Parameters fail SvMF checks: %s", paste(names(checks)[!checks], collapse = ", ")))
+    stop(sprintf("Parameters fail SvMF checks: %s", paste(names(checks)[!checks], collapse = ", ")))
   }
   return(NULL)
 }
 
+#' @describeIn SvMFparams The parameterisation of a SvMF that uses a kappa, mean, a_1, and a symmetric positive definite matrix `V` with `det(V) = 1`. This parameterisation is defined in equations 8 and 9 of Scealy and Wood 2019.
+#' The log-likelihood using this parameterisation is in equation 11 of Scealy and Wood 2019.
+SvMFmuV <- function(k, m, a1, V){
+  obj <- list(k = k, m = m, a1 = a1, V = V)
+  class(obj) <- c("SvMFmuV", class(obj))
+  return(obj)
+}
+SvMFmuV_check <- function(obj){
+  list2env(obj, envir = environment())
+  checks <- c(
+    `kappa positive` = k > 0,
+    `a1 positive` = a1 > 0,
+    `m size` = (vnorm(m) - 1)^2 <  sqrt(.Machine$double.eps),
+    `V square` = ncol(V) == nrow(V),
+    `V size` = ncol(V) == length(m) - 1,
+    `V symmetric` = isSymmetric(V),
+    `det(V)` = (det(V) - 1)^2 < sqrt(.Machine$double.eps)
+  )
+  if (any(!checks)){
+    stop(sprintf("Parameters fail checks: %s", paste(names(checks)[!checks], collapse = ", ")))
+  }
+  return(NULL)
+}
 
+# conversion
+SvMF_cann2muV <- function(obj){
+  list2env(obj, envir = environment())
+  m <- G[, 1]
+  m1 <- m[1]
+  mL <- m[-1]
+  Hstar <- rbind(mL, (1/(1+m1)) * mL %*% t(mL) - diag(1, length(mL)))
+  Kstar <- t(Hstar) %*% G[, -1] #this solves Hstar %*% Kstar = G[, -1] because by orthogonality of H, t(Hstar) %*% Hstar = I.
+  V <- Kstar %*% diag(a[-1]^2) %*% t(Kstar)
+  SvMFmuV(k, m, a[1], V)
+}
+SvMF_muV2cann <- function(obj){
+  list2env(obj, envir = environment())
+  m1 <- m[1]
+  mL <- m[-1]
+  Hstar <- rbind(mL, (1/(1+m1)) * mL %*% t(mL) - diag(1, length(mL)))
+  es <- eigen(V)
+  Kstar <- es$vectors
+  SvMFcann(k, c(a1, sqrt(es$values)), G = cbind(m, Hstar %*% Kstar))
+}
---
+++ tests/testthat/test-SvMF_parameterisations.R
@@ -0,0 +1,29 @@
+test_that("SvMFcann() creates objects that pass checks", {
+  p <- 5
+  set.seed(1)
+  a <- runif(p)
+  a[p] <- a[p]/prod(a[-1])
+  set.seed(1); Gamma <- mclust::randomOrthogonalMatrix(p, p)
+  obj <- SvMFcann(0.5, a, Gamma)
+  expect_silent(SvMFcann_check(obj))
+})
+
+test_that("SvMF_cann2muV and reversible passes checks", {
+  p <- 5
+  set.seed(1)
+  a <- runif(p)
+  a[p] <- a[p]/prod(a[-1])
+  a <- c(a[1], sort(a[-1], decreasing = TRUE))
+  set.seed(1); Gamma <- mclust::randomOrthogonalMatrix(p, p)
+  obj <- SvMFcann(0.5, a, Gamma)
+  
+  obj2 <- SvMF_cann2muV(obj)
+  expect_silent(SvMFmuV_check(obj2))
+  
+  expect_equal(SvMF_muV2cann(obj2)[1:2], obj[1:2]) #up to directions of G[, -1]!
+  expect_equal(abs(SvMF_muV2cann(obj2)[3]), abs(obj[3])) #up to directions of G[, -1]!
+})
+
+
+
+

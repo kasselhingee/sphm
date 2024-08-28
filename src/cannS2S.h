c4b675b84d397c1ff175691683bce094607d5227
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-08-28 15:28:26 +1000

    cannS2S for cpp started

---
+++ src/cannS2S.h
@@ -0,0 +1,44 @@
+# ifndef CANNS2S_H
+# define CANNS2S_H
+
+# include <sphm_forward.h>
+# include <scorematchingad_forward.h>
+# include <RcppEigen.h>
+# include <Rcpp.h>
+
+// Define the templated C++ struct for the cannS2S parametetrisation
+template <typename T>
+struct OmegaS2Scpp {
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> P;
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Q;
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> B;
+
+    cannS2Scpp(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> P_, Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Q_, Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> B_)
+        : P(P_), Q(Q_), B(B_) {}
+};
+
+// convert from OmegaS2S object to cannS2S object
+// Convert OmegaS2Scpp to cannS2Scpp
+template <typename T>
+cannS2Scpp<T> Omega2cann(const OmegaS2Scpp<T>& obj) {
+    // Perform SVD on the Omega matrix
+    Eigen::JacobiSVD<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>> svd(obj.Omega, Eigen::ComputeThinU | Eigen::ComputeThinV);
+    Rcpp::Rcout << svd.matrixU() << std::endl;
+    Rcpp::Rcout << svd.matrixV() << std::endl;
+    
+    // Create Q by combining q1 with the V matrix from SVD
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Q(obj.q1.size(), 1 + svd.matrixV().cols());
+    Q << obj.q1, svd.matrixV();
+
+    // Create P by combining p1 with the U matrix from SVD
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> P(obj.p1.size(), 1 + svd.matrixU().cols());
+    P << obj.p1, svd.matrixU();
+
+    // Create B as a diagonal matrix with the singular values, excluding the last one
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> B = svd.singularValues().head(svd.singularValues().size() - 1).asDiagonal();
+
+    // Return a cannS2Scpp object with P, Q, and B
+    return cannS2Scpp<T>(P, Q, B);
+}
+
+# endif

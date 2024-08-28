4a3e54a7f7fc5b7235f95ef3ad9998fe16479bd1
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-08-28 10:09:53 +1000

    part way through drafting test of aligned_mean

--- tests/testthat/test-SvMF_ll.R
+++ tests/testthat/test-SvMF_ll.R
@@ -20,3 +20,41 @@ test_that("SvMF_ll is the same using either parameterisatiion", {
   ll_cpp <- do.call(ldSvMF_muV, c(list(y = y), obj2))
   expect_equal(ll_cpp, ll_muV)
 })
+
+test_that("ll using mean link in C++ matches R", {
+  p <- 3
+  q <- 5
+  # data generating parameters:
+  set.seed(1)
+  P <- mclust::randomOrthogonalMatrix(p, p)
+  set.seed(2)
+  Q <- mclust::randomOrthogonalMatrix(q, p)
+  set.seed(3)
+  B <- diag(sort(runif(p-1), decreasing = TRUE))
+  omegapar <- as_OmegaS2S(cannS2S(P,Q,B))
+  
+  #generate covariates uniformly on the sphere
+  set.seed(4)
+  x <- matrix(rnorm(1000*q), nrow = 1000)
+  x <- sweep(x, 1, apply(x, 1, vnorm), FUN = "/")
+  
+  ymean <- meanlinkS2S(x = x, paramobj = omegapar)
+  
+  # generate noise
+  if (!requireNamespace("movMF", quietly = TRUE)){skip("Need movMF package")}
+  set.seed(5)
+  y <- t(apply(ymean, 1, function(mn){movMF::rmovMF(1, 10*mn)}))
+  
+  # log-likelihood via R
+  k <- 10
+  a <- c(1, seq(5, 0.2, length.out = p-1))
+  # fix a P matrix
+  P <- diag(1, p)
+  ld <- rep(NA, nrow(y))
+  for (i in 1:nrow(y)){
+    G <- alignedG(ymean[i, ], P)
+    ld[i] <- ldSvMF_cann(y[i, , drop = FALSE], k, a, G)
+  }
+  
+  
+})

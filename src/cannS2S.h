df957d64fd46ac6cb411c4271e127bf506df4c0d
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-08-30 17:40:03 +1000

    Omega2cann confirmed working

--- src/cannS2S.h
+++ src/cannS2S.h
@@ -20,6 +20,8 @@ struct cannS2Scpp {
 // Convert OmegaS2Scpp to cannS2Scpp
 template <typename T>
 cannS2Scpp<T> Omega2cann(const OmegaS2Scpp<T>& obj) {
+    int p = obj.p1.size();
+    int q = obj.q1.size();
     //try using isfinite overloading from cppad
     CppAD::AD<double> test_var = 1.0;
     // Explicitly call the functions to force instantiation
@@ -28,16 +30,14 @@ cannS2Scpp<T> Omega2cann(const OmegaS2Scpp<T>& obj) {
 
     // Perform SVD on the Omega matrix
     Eigen::JacobiSVD<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>> svd(obj.Omega, Eigen::ComputeThinU | Eigen::ComputeThinV);
-    Rcpp::Rcout << svd.matrixU() << std::endl;
-    Rcpp::Rcout << svd.matrixV() << std::endl;
     
     // Create Q by combining q1 with the V matrix from SVD
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Q(obj.q1.size(), 1 + svd.matrixV().cols());
-    Q << obj.q1, svd.matrixV();
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> Q(obj.q1.size(), p);
+    Q << obj.q1, svd.matrixV().leftCols(p-1);
 
     // Create P by combining p1 with the U matrix from SVD
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> P(obj.p1.size(), 1 + svd.matrixU().cols());
-    P << obj.p1, svd.matrixU();
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> P(obj.p1.size(), p);
+    P << obj.p1, svd.matrixU().leftCols(p-1);
 
     // Create B as a diagonal matrix with the singular values, excluding the last one
     Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> B = svd.singularValues().head(svd.singularValues().size() - 1).asDiagonal();

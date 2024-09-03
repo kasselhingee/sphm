936db98a5eba571ba31d98a98f68e464c6177644
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-09-03 13:04:43 +1000

    getting error, maybe it is the comma?

--- src/function_map.h
+++ src/function_map.h
@@ -7,6 +7,7 @@
 // defining a function map that will convert string names to a pointer to the function
 std::map<std::string, generalfunction> function_map = {
    {"pobjS2Scpp", &pobjS2Scpp},
+   {"wrap_OmegaS2S_constraints", &wrap_OmegaS2S_constraints}
 };
 
 

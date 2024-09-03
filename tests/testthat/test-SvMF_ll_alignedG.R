7ce435e9070adfc842bb68e7958fd3182d531ef7
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-09-03 16:14:50 +1000

    alignedG tests moved to specific file

---
+++ tests/testthat/test-SvMF_ll_alignedG.R
@@ -0,0 +1,77 @@
+test_that("ll using alignedGmean link in C++ matches R", {
+  p <- 3
+  q <- 5
+  # data generating parameters:
+  set.seed(1)
+  P <- mclust::randomOrthogonalMatrix(p, p)
+  set.seed(2)
+  Q <- mclust::randomOrthogonalMatrix(q, p)
+  set.seed(3)
+  B <- diag(sort(runif(p-1), decreasing = TRUE))
+  omegapar <- as_OmegaS2S(cannS2S(P,Q,B))
+  
+  #generate covariates uniformly on the sphere
+  set.seed(4)
+  x <- matrix(rnorm(1000*q), nrow = 1000)
+  x <- sweep(x, 1, apply(x, 1, vnorm), FUN = "/")
+  
+  ymean <- meanlinkS2S(x = x, paramobj = omegapar)
+  
+  # generate noise
+  if (!requireNamespace("movMF", quietly = TRUE)){skip("Need movMF package")}
+  set.seed(5)
+  y <- t(apply(ymean, 1, function(mn){movMF::rmovMF(1, 10*mn)}))
+  
+  # log-likelihood via R
+  k <- 10
+  a <- c(1, seq(5, 0.2, length.out = p-1))
+  # fix a P matrix, may as well be the true one
+  ld <- rep(NA, nrow(y))
+  for (i in 1:nrow(y)){
+    G <- alignedG(ymean[i, ], P)
+    ld[i] <- ldSvMF_cann(y[i, , drop = FALSE], k, a, G)
+  }
+  
+  ldcpp <- ll_SvMF_S2S_alignedGmean(OmegaS2S_vec(omegapar), dyn = c(k, a, as.vector(P)), p, cbind(y, x))
+  expect_equal(ld, ldcpp)
+  
+  # compute likelihood when a2, ... is the independent vector and P, k is fixed
+  ldcpp <- ll_SvMF_S2S_alignedGa(a[-1], c(k, a[1]), c(p, OmegaS2S_vec(omegapar)), cbind(y, x))
+  expect_equal(ld, ldcpp)
+
+  # compute likelihood when k is the only independent vector
+  ldcpp <- ll_SvMF_S2S_alignedGk(k, c(OmegaS2S_vec(omegapar), a, as.vector(P)), p, cbind(y, x))
+  expect_equal(ld, ldcpp)
+})
+
+
+test_that("maximum likelihood for alignedP link", {
+  p <- 3
+  q <- 5
+  # data generating parameters:
+  set.seed(1)
+  P <- mclust::randomOrthogonalMatrix(p, p)
+  set.seed(2)
+  Q <- mclust::randomOrthogonalMatrix(q, p)
+  set.seed(3)
+  B <- diag(sort(runif(p-1), decreasing = TRUE))
+  omegapar <- as_OmegaS2S(cannS2S(P,Q,B))
+  
+  #generate covariates uniformly on the sphere
+  set.seed(4)
+  x <- matrix(rnorm(1000*q), nrow = 1000)
+  x <- sweep(x, 1, apply(x, 1, vnorm), FUN = "/")
+  
+  ymean <- meanlinkS2S(x = x, paramobj = omegapar)
+  
+  # generate noise according to SvMF
+  k <- 10
+  a <- c(1, seq(5, 0.2, length.out = p-1))
+  set.seed(5)
+  y <- t(apply(ymean, 1, function(mn){
+    G <- alignedG(mn, P)
+    rSvMF(1, SvMFcann(k, a, G))
+  }))
+  
+
+})

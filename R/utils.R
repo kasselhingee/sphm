2b54f73a0d562513683a0dd6c5af3d1eb7932d42
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-09-16 10:18:59 +1000

    add standardise() function

--- R/utils.R
+++ R/utils.R
@@ -17,3 +17,30 @@ cayley <- function(x){
   return(P)
 }
 
+#' Standardise Data to North Pole
+#' @description Rotation according to the eigenvectors of the second moment of the data
+#' projected perpendicular to the mean direction. Standardised data have mean of c(1, 0, 0,...). See Scealy and Wood 2019 for details.
+#' @param y Data on the sphere. Each row is a data point in Cartesian coordinates.
+#' @param G Axes of the second moment matrix of the data, projected so that the first column is the global mean.
+#' @details Each returned data point is `t(G)` of the original data point, where `G` is computed by `standardise_mat()`.
+#' @export
+standardise <- function(y, G = standardise_mat(y)){
+  ystd <- y %*% G
+  ystd <- unname(ystd)
+  return(ystd)
+}
+
+#' @describeIn standardise
+#' @export
+standardise_mat <- function(y){
+  p <- ncol(y)
+  mn <- colMeans(y)
+  mn <- mn/sqrt(sum(mn^2))
+  mnproj <- diag(1, p) - mn %*% t(mn)
+
+  mom2 <- t(y) %*% y #quickly calculates the sum of projection matrices of rows of y
+  projmom2 <- mnproj %*% mom2 %*% mnproj
+
+  Ghat <- cbind(mn, eigen(projquasicov)$vectors[, 1:(p-1)])
+  return(Ghat)
+}

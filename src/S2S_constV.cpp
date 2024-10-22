c8e3f021880f5ccb6c41f0c4664c606956b1e6ae
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-10-23 08:54:04 +1100

    ull_S2S_constV is compiling

--- src/S2S_constV.cpp
+++ src/S2S_constV.cpp
@@ -1,1 +1,42 @@
+#include "S2S_constV.h"
+#include "meanlinkS2S.h"
+#include "uldSvMF.h"
+
+mata1 JuppRmat(veca1 const & y1, veca1 const & y2){
+  veca1 sum = y1 + y2;
+  a1type denom = 1.0 + y1.dot(y2);//(y1.transpose() * y2).coeff(0,0)
+  mata1 ident = mata1::Identity(y1.size(), y1.size());
+  mata1 out = ((sum * sum.transpose()).array() / denom).matrix() - ident;
+  return out;
+}
+
+
+veca1 ull_S2S_constV(mata1 y, mata1 x, OmegaS2Scpp<a1type> om, a1type k, a1type a1, veca1 aremaining, mata1 Gstar){
+  int p = om.p1.size();
+  //check that ncol(y) == p
+  if (y.cols() != p){Rcpp::stop("width of y does not equal length of p1");}
+
+  // project Omega matrix to satisfy orthogonality to p1 and q1
+  OmegaS2Scpp<a1type> om_projected = OmegaS2Sproj(om);
+  veca1 omvec_projected = OmegaS2Scpp_vec(om_projected);
+
+  //get mean
+  mata1 ypred;
+  ypred = meanlinkS2Scpp(x, omvec_projected, p);
+
+  //evaluate SvMF density of each observation
+  veca1 ld(y.rows());
+  mata1 G(p, p);
+  G.col(0) = om_projected.p1;
+  veca1 a(p);
+  a(0) = a1;
+  a.segment(1, p-1) = aremaining;
+  for (int i = 0; i < y.rows(); ++i){
+    G.block(0, 1, p, p-1) = JuppRmat(om_projected.p1, y.row(i)) * Gstar;
+    ld(i) = uldSvMF_cann(y.row(i), k, a, G)(0);
+  }
+  return ld;
+}
+
+
 
--- src/S2S_constV.h
+++ src/S2S_constV.h
@@ -2,30 +2,32 @@
 #define S2S_constV_H
 
 #include <scorematchingad_forward.h> //includes RcppEigenForward
+#include <utils/pADFun.h>
 #include "OmegaS2S.h"
 
 // function that accepts all the major arguments in usual (non-vector) format
 // for a single observation y with covariates x and return the ull for that observation, parallel transporting axes Gstar to the mean using Jupp's method from base being the first column of P.
-a1type ull_S2S_constV(veca1 y, veca1 x, OmegaS2Scpp<a1type> om, a1type k, a1type a1, veca1 aremaining, veca1, mata1 Gstar);
+// will include projection of om
+veca1 ull_S2S_constV(mata1 y, mata1 x, OmegaS2Scpp<a1type> om, a1type k, a1type a1, veca1 aremaining, mata1 Gstar);
 
 //tape for mean wrt mean with the base point P[,1] fixed for calculating the axes. Returned tape should have dynamic parameters of: k, a1, aremaining, as.vector(Gstar).
-pADFFun tape_ull_S2S_constV_mean(veca1 & omvec, a1type k, a1type a1, veca1 aremaining, mata1 Gstar, vecd & p_in, matd & yx);
+pADFun tape_ull_S2S_constV_mean(veca1 & omvec, a1type k, a1type a1, veca1 aremaining, mata1 Gstar, vecd & p_in, matd & yx);
 
 // tape for axes
 // axes parameterised by the Cayley transform of K* in H*(P[,1])K*
 // the Cayley transform will work okay so long as K* is reasonably close to the basis given by H*. If P[,1] is reasonably close to north pole (due to standardisation of the data) and the axes reasonably close to the ambient basis axes, then K* will be reasonably close to the identity. This is good for using Cayley transforms.
 //returned tape should have as dynamic parameters: P1, as.vector(om), k, a1, aremaining
-pADFFun tape_ull_S2S_constV_axes(veca1 & vecCayaxes, OmegaS2Scpp<a1type> om, a1type k, a1type a1, veca1 aremaining, vecd & p_in, matd & yx);
+pADFun tape_ull_S2S_constV_axes(veca1 & vecCayaxes, OmegaS2Scpp<a1type> om, a1type k, a1type a1, veca1 aremaining, vecd & p_in, matd & yx);
 
 // tape for aremaining. Like _mean but everything else constant
-pADFFun tape_ull_S2S_constV_aremaining(veca1 & aremaining, OmegaS2Scpp<a1type> om, a1type k, a1type a1, mata1 Gstar, vecd & p_in, matd & yx);
+pADFun tape_ull_S2S_constV_aremaining(veca1 & aremaining, OmegaS2Scpp<a1type> om, a1type k, a1type a1, mata1 Gstar, vecd & p_in, matd & yx);
 
 
 // tape for k
-pADFFun tape_ull_S2S_constV_k(veca1 k, OmegaS2Scpp<a1type> om, a1type a1, veca1 & aremaining, mata1 Gstar, vecd & p_in, matd & yx);
+pADFun tape_ull_S2S_constV_k(veca1 k, OmegaS2Scpp<a1type> om, a1type a1, veca1 & aremaining, mata1 Gstar, vecd & p_in, matd & yx);
 
 // experimental: tape for everything all together except a1
 // make the independent values omvec, k, aremaining and vecCayaxes,
-pADFFun tape_ull_S2S_constV_all(veca1 & omvec, a1type k, a1type a1, veca1 aremaining, mata1 Gstar, vecd & p_in, matd & yx);
+pADFun tape_ull_S2S_constV_all(veca1 & omvec, a1type k, a1type a1, veca1 aremaining, mata1 Gstar, vecd & p_in, matd & yx);
 #endif
 

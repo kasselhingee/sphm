819d795340d42c3c6131674c06d97b8671bfc856
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-10-23 09:27:24 +1100

    another bug found

--- src/S2S_constV.cpp
+++ src/S2S_constV.cpp
@@ -27,11 +27,11 @@ veca1 ull_S2S_constV(mata1 y, mata1 x, OmegaS2Scpp<a1type> om, a1type k, a1type 
   //evaluate SvMF density of each observation
   veca1 ld(y.rows());
   mata1 G(p, p);
-  G.col(0) = om_projected.p1;
   veca1 a(p);
   a(0) = a1;
   a.segment(1, p-1) = aremaining;
   for (int i = 0; i < y.rows(); ++i){
+     G.col(0) = ypred.row(i);
     G.block(0, 1, p, p-1) = JuppRmat(om_projected.p1, ypred.row(i)) * Gstar;
     ld(i) = uldSvMF_cann(y.row(i), k, a, G)(0);
   }

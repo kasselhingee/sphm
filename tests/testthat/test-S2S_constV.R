bc2ff9b47f4e0d330d104d9070e288a416efa129
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-10-23 07:06:49 +1100

    testing and drafting moved a bit further

---
+++ tests/testthat/test-S2S_constV.R
@@ -0,0 +1,38 @@
+test_that("maximum likelihood for parallel axes per Jupp's path", {
+  p <- 3
+  q <- 5
+  # data generating parameters:
+  set.seed(1)
+  P <- mclust::randomOrthogonalMatrix(p, p)
+  set.seed(2)
+  Q <- mclust::randomOrthogonalMatrix(q, p)
+  set.seed(3)
+  B <- diag(sort(runif(p-1), decreasing = TRUE))
+  omegapar <- as_OmegaS2S(cannS2S(P,Q,B))
+  
+  #generate covariates uniformly on the sphere
+  set.seed(4)
+  x <- matrix(rnorm(1000*q), nrow = 1000)
+  x <- sweep(x, 1, apply(x, 1, vnorm), FUN = "/")
+  
+  ymean <- meanlinkS2S(x = x, paramobj = omegapar)
+ 
+  # generate noise
+  # step 1: axes defined at P[, 1], orthogonal to P[, 1]
+  set.seed(5)
+  Kstar <- mclust::randomOrthogonalMatrix(p-1, p-1)
+  Gstar <- getHstar(P[ ,1]) %*% Kstar
+  # step 2: assume concentration and scales are constant
+  k <- 30
+  a <- c(1, seq(5, 0.2, length.out = p-1))
+  a[-1] <- a[-1]/prod(a[-1])^(1/(p-1))
+  #step 3: for each location define G as mean, Jupp transport of Gstar columns
+  # then simulate from a SvMF
+  set.seed(6)
+  y <- t(apply(ymean, 1, function(mn){
+    G <- cbind(mn, JuppRmat(P[,1], mn) %*% Gstar)
+    rSvMF(1, SvMFcann(k, a, G))
+  }))
+  
+  
+})

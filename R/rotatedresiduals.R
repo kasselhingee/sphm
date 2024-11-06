351cf4a7db1b69e9267afdf4468501fe5b08c2b1
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-11-06 12:34:01 +1100

    Amaral matrix copied from Rsnippets

--- R/rotatedresiduals.R
+++ R/rotatedresiduals.R
@@ -22,3 +22,14 @@ cruderesid <- function(y, ypred){
 JuppRmat <- function(y, base){
   (y+base) %*% t(y+base)/(1+drop(y%*%base)) - diag(1, length(y))
 }
+
+
+rotationmat_amaral  <- function(a, b){ #assumes a and b are unit vectors
+  ab <- (a %*% b)[[1]]
+  alpha <- acos(ab)
+  c <- b - a*ab
+  c <- c/sqrt(c %*% c)[[1]]
+  A <- a%o%c - c%o%a
+  Q = diag(length(a)) + sin(alpha)*A + (cos(alpha) - 1)*(a%o%a + c%o%c)
+  return(Q)
+}

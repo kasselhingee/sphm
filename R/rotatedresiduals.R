c2cafb9ae9ec7a88e35f0c8469e37d897bfb6b7e
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-11-06 12:43:11 +1100

    rotatedresiduals() now use geodesic path by default

--- R/rotatedresiduals.R
+++ R/rotatedresiduals.R
@@ -1,13 +1,14 @@
 #' Jupp's Rotated Residuals
 
 #' @export
-rotatedresid <- function(y, ypred, base){
+rotatedresid <- function(y, ypred, base, path = "geo"){
   #crude residuals
   cresids <- cruderesid(y, ypred)
 
   # rotate them
+  transportmat <- if (path == "Jupp"){JuppRmat} else {rotationmat_amaral}
   rresids <- t(sapply(1:nrow(y), function(i){
-    JuppRmat(ypred[i, ], base) %*%  cresids[i, ]
+    transportmat(ypred[i, ], base) %*%  cresids[i, ]
   }))  
   return(rresids)
 }
@@ -23,11 +24,11 @@ JuppRmat <- function(y, base){
   (y+base) %*% t(y+base)/(1+drop(y%*%base)) - diag(1, length(y))
 }
 
-
-rotationmat_amaral  <- function(a, b){ #assumes a and b are unit vectors
-  ab <- (a %*% b)[[1]]
+# Rotation of tangent vectors from base to y
+rotationmat_amaral  <- function(base, a){ #assumes a and b are unit vectors
+  ab <- (a %*% base)[[1]]
   alpha <- acos(ab)
-  c <- b - a*ab
+  c <- base - a*ab
   c <- c/sqrt(c %*% c)[[1]]
   A <- a%o%c - c%o%a
   Q = diag(length(a)) + sin(alpha)*A + (cos(alpha) - 1)*(a%o%a + c%o%c)

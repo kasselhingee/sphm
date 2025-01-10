33bf87ca996ca719b30b7cd49005689792b1d721
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-01-10 21:36:03 +1100

    I think I've updated the Omega to reflect that it creates a p-length vector

--- R/parameterisations.R
+++ R/parameterisations.R
@@ -8,7 +8,7 @@
 #' @param Be Scaling matrix for Euclidean covariates: a (p-1) x (p-1) diagonal matrix with elements between zero and one ordered in decreasing size. `NULL` if no Euc covariates.
 #' @param Qs The qs x p rotation-like matrix `R_s` for rotating the spherical covariate vector. `NULL` if no Sph covariates.
 #' @param Qe The qe x p rotation-like matrix `R_e` for rotating the Euclidean covariate vector. `NULL` if no Euc covariates.
-#' @param ce The additive offset \eqn{c_e} for Euclidean covariates only. Vector of length p. `NULL` if no Euc covariates.
+#' @param ce The additive offset \eqn{c_e} for Euclidean covariates only. Vector of length p. If an element of `Be` is 0, then the convention will be for th corresponding element of `ce` to be zero. `NULL` if no Euc covariates.
 #' @details
 #' # Cannonical Parameterisation
 #' The `P`, `Bs`, `Be`, `Qs`, `Qe` and `ce` is slightly more flexible than Shogo's link function with both Euclidean covariates and a spherical covariate that matches Remark 1 of Manuscript (Nov 29, 2024).
@@ -17,7 +17,7 @@
 #' Andy's link function for Euclidean covariates needs an additional scaling \eqn{b_{im}} parameter for the imaginary component Andy's link function to be parameterised. It will also need `ce = 0` and `Bs` and `Qs` will be ignored since spherical covariates not incorporated yet.
 #' 
 #' # Omega Parameterisation
-#' The link functions are simplified by writing \eqn{\Omega_s = P^* B_s {Q_s^*}^T} and \eqn{\Omega_e = P^* B_e {Q_e^*}^T}, and finally \eqn{\Omega = [\Omega_s \,\, \Omega_e]}.
+#' The link functions are simplified by writing \eqn{\Omega_s = P^* B_s {Q_s^*}^T} and \eqn{\Omega_e = P^* B_e {Q_e^*}^T}, \eqn{\Omega = [\Omega_s \,\, \Omega_e]}, and \eqn{'PBc_e' = P^* B_e c_e[-1]}.
 #' This parameterisation helps optimisation as optimisation in Stiefel manifolds is harder than other spaces, and also reflects the sign ambiguity of columns of P with the matching columns of `Qe` and `Qs`.
 #' 
 NULL
@@ -48,22 +48,26 @@ as_mnlink_cann <- function(obj){
 #' @param qs1 First column of the Qs matrix (vector of length `qs`). `NULL` if no Sph covariates.
 #' @param Omega A `p` by `qe + qs` matrix representing 
 #' \deqn{\Omega = [\Omega_s \Omega_e] = [P^* B_s {Q_s^*}^T \,   P^* B_e {Q_e^*}^T]}
+#' @param PBce The p-length vector \eqn{P^* B_e c_e[-1]}.
+#' @param ce1 The first element of \eqn{c_e}.
 NULL
 
 OmegaS2S <- function(p1, q1, Omega, check = TRUE){
   mnlink_Omega(p1 = p1, qs1 = q1, Omega = Omega, check = check)
 }
 
-mnlink_Omega <- function(p1, qs1 = vector("numeric", 0), Omega, qe1 = vector("numeric", 0), ce = vector("numeric", 0), check = TRUE){
+mnlink_Omega <- function(p1, qs1 = vector("numeric", 0), Omega, qe1 = vector("numeric", 0), ce1 = vector("numeric", 0), PBce = vector("numeric", 0), check = TRUE){
   if (is.null(qs1)){obj$qs1 <- vector("numeric", 0)}
   if (is.null(qe1)){obj$qe1 <- vector("numeric", 0)}
-  if (is.null(ce)){obj$ce <- vector("numeric", 0)}
+  if (is.null(ce1)){obj$ce1 <- vector("numeric", 0)}
+  if (is.null(PBce)){obj$PBce <- vector("numeric", 0)}
   obj <- list(
     p1 = p1,
     qs1 = qs1,
     qe1 = qe1,
     Omega = Omega,
-    ce = ce
+    ce1 = ce1
+    PBce = PBce
   )
   class(obj) <- c("mnlink_Omega", class(obj))
   if (check) {mnlink_Omega_check(obj)}
@@ -87,7 +91,8 @@ mnlink_Omega_vec <- function(obj){
   qs1 <- obj$qs1
   qe1 <- obj$qe1
   Omega <- obj$Omega
-  ce <- obj$ce
+  ce1 <- obj$ce1
+  PBce <- obj$PBce
   Omegavec <- as.vector(Omega)
   names(Omegavec) <- as.vector(
       outer(seq.int(1, length.out = length(p1)), 
@@ -98,8 +103,9 @@ mnlink_Omega_vec <- function(obj){
   names(p1) <- paste0("p1_", seq.int(1, length.out = length(p1)), recycle0 = TRUE)
   names(qs1) <- paste0("qs1_", seq.int(1, length.out = length(qs1)), recycle0 = TRUE)
   names(qe1) <- paste0("qe1_", seq.int(1, length.out = length(qe1)), recycle0 = TRUE)
-  names(ce) <- paste0("ce_", seq.int(1, length.out = length(ce)), recycle0 = TRUE)
-  out <- c(p1, qs1, qe1, Omegavec, ce)
+  names(ce1) <- paste0("ce", seq.int(1, length.out = length(ce1)), recycle0 = TRUE)
+  names(PBce) <- paste0("PBce_", seq.int(1, length.out = length(PBce)), recycle0 = TRUE)
+  out <- c(p1, qs1, qe1, Omegavec, ce1, PBce)
   class(out) <- "mnlink_Omega_vec"
   return(out)
 }
@@ -109,10 +115,10 @@ mnlink_Omega_vec <- function(obj){
 #' @param p The dimension of the response (The dimension of covariates will be infered from `p`).
 #' @param qe Number of Euclidean covariates
 mnlink_Omega_unvec <- function(vec, p, qe = 0, check = TRUE){
-  # length of vec = p + qs + qe + p*(qs + qe) + p*(qe>0)
-  # l - p*(qe>0) - qe - p*qe = qs + p*qs
-  # (l - p*(qe>0) - qe - p*qe)/(1+p) = qs
-  qs <- (length(vec) - p - p*(qe>0) - qe - p*qe)/(1+p)
+  # length of vec = p + qs + qe + p*(qs + qe) + (qe>0) + p*(qe>0)
+  # l - (p+1)*(qe>0) - qe - p*qe = qs + p*qs
+  # (l - (p+1)*(qe>0) - qe - p*qe)/(1+p) = qs
+  qs <- (length(vec) - p - (p+1)*(qe>0) - qe - p*qe)/(1+p)
   stopifnot(qs == as.integer(qs))
   names(vec) <- NULL
   
@@ -120,7 +126,8 @@ mnlink_Omega_unvec <- function(vec, p, qe = 0, check = TRUE){
            qs1 = vec[p + seq.int(1, length.out = qs)],
            qe1 = vec[p + qs + seq.int(1, length.out = qe)],
            Omega = matrix(vec[p + qs + qe + seq.int(1, length.out = p*(qe + qs))], nrow = p, ncol = qs + qe, byrow = FALSE),
-           ce = vec[p + qs + qe + p*(qs + qe) + seq.int(1, length.out = p * (qe > 0))],
+           ce1 = vec[p + qs + qe + p*(qs + qe) + seq.int(1, length.out = (qe > 0))],
+           PBce = vec[p + qs + qe + p*(qs + qe) + (qe>0) + seq.int(1, length.out = p * (qe > 0))],
            check = check)
 }
 
@@ -140,15 +147,17 @@ cann2Omega <- function(obj, check = TRUE){
   p1 <- obj$P[, 1]
   qs1 <- obj$Qs[, 1]
   qe1 <- obj$Qe[, 1]
-  Omega_s <- Omega_e <- NULL
+  Omega_s <- Omega_e <- ce1 <- PBce <- NULL
   if (!is.null(qs1)){
     Omega_s <- obj$P[,-1] %*% obj$Bs %*% t(obj$Qs[, -1])
   }
   if (!is.null(qe1)){
     Omega_e <- obj$P[,-1] %*% obj$Be %*% t(obj$Qe[, -1])
+    ce1 <- obj$ce[1]
+    PBce <- obj$P[,-1] %*% obj$Be %*% obj$ce[-1]
   }
   Omega <- cbind(Omega_s, Omega_e)
-  return(mnlink_Omega(p1, qs1 = qs1, Omega = Omega, qe1 = qe1, ce = obj$ce, check = FALSE))
+  return(mnlink_Omega(p1, qs1 = qs1, Omega = Omega, qe1 = qe1, ce1 = ce1, PBce = PBce, check = FALSE))
 }
 
 #' # Warning
@@ -160,7 +169,7 @@ Omega2cann <- function(obj, check = TRUE){
   P <- cbind(obj$p1, svdres$u)
   
   # the rest uses the SVD of Omega as written in the Euclidean link document
-  Qs <- Qe <- Bs <- Be <- NULL
+  Qs <- Qe <- Bs <- Be <- ce <- NULL
   if (length(obj$qs1) > 0){
     Qs_unnorm <- svdres$v[seq.int(1, length.out = length(obj$qs1)), , drop = FALSE]
     Qs_norms <- sqrt(colSums(Qs_unnorm^2))
@@ -174,9 +183,11 @@ Omega2cann <- function(obj, check = TRUE){
     Qestar <- t(t(Qe_unnorm)/ Qe_norms)
     Qe <- cbind(obj$qe1, Qestar)
     Be <- diag(Qe_norms * svdres$d[-nrow(obj$Omega)])
+    ce <- c(obj$ce1, diag(1/diag(Be)) %*% t(P[,-1]) %*% obj$PBce)
+    ce[-1][abs(diag(Be)) < .Machine$double.eps] <- 0  #if element of B very close to 0, set the element of ce to 0, rather than NaN
   }
   
-  mnlink_cann(P, Bs = Bs, Qs = Qs, Be = Be, Qe = Qe, ce = obj$ce, check = check)
+  mnlink_cann(P, Bs = Bs, Qs = Qs, Be = Be, Qe = Qe, ce = ce, check = check)
 }
 
 mnlink_cann_check <- function(obj){
@@ -227,12 +238,16 @@ mnlink_Omega_check <- function(obj){
   if (is.null(obj$p1)){stop("p1 should be non-null")}
   if (is.null(obj$qs1)){stop("qs1 should be non-null")}
   if (is.null(obj$qe1)){stop("qe1 should be non-null")}
-  if (is.null(obj$ce)){stop("ce should be non-null")}
+  if (is.null(obj$ce1)){stop("ce1 should be non-null")}
+  if (is.null(obj$PBce)){stop("PBce should be non-null")}
 
   stopifnot(length(obj$p1) == nrow(obj$Omega))
   stopifnot(length(obj$qs1) + length(obj$qe1) == ncol(obj$Omega))
-  stopifnot( ( (length(obj$qe1) > 0) + (length(obj$ce) > 0)) %in% c(0, 2))
-  if(length(obj$qe1) > 0){stopifnot(length(obj$ce) == length(obj$p1))}
+  stopifnot( ( (length(obj$qe1) > 0) + (length(obj$ce1) > 0) +  (length(obj$PBce) > 0)) %in% c(0, 3))
+  if(length(obj$qe1) > 0){
+    stopifnot(length(obj$ce1) == 1)
+    stopifnot(length(obj$PBce) == length(obj$p1))
+  }
   
   vals <- mnlink_Omega_check_numerical(obj)
   good <- (vals < sqrt(.Machine$double.eps))
@@ -268,7 +283,8 @@ mnlink_Omega_check_numerical <- function(obj){ #uses squared values for smoothne
   if (qe > 0){
     checkvals <- c(
       qe1sizediff = (vnorm(obj$qe1) - 1)^2,
-      Omegaqe1 = (obj$Omega[, qs + seq.int(1, qe)] %*% obj$qe1)^2
+      Omegaqe1 = (obj$Omega[, qs + seq.int(1, qe)] %*% obj$qe1)^2,
+      p1PBce = (t(obj$p1) %*% obj$PBce)^2
     )
   }
   return(checkvals)
@@ -278,8 +294,8 @@ mnlink_Omega_check_numerical <- function(obj){ #uses squared values for smoothne
 Omega_proj <- function(obj){
   stopifnot(inherits(obj, "mnlink_Omega"))
   # first project orthogonal to p1 (needs p1 unit vector)
-  p1 <- obj$p1/vnorm(obj$p1)
-  newOmega <- obj$Omega -  (p1 %*% t(p1)) %*% obj$Omega
+  obj$p1 <- obj$p1/vnorm(obj$p1)
+  newOmega <- obj$Omega -  (obj$p1 %*% t(obj$p1)) %*% obj$Omega
   # now t(p1) %*% newOmega = 0
   
   Omega_s <- Omega_e <- NULL
@@ -288,10 +304,11 @@ Omega_proj <- function(obj){
     Omega_s <- newOmega[, seq.int(1, length.out = length(obj$qs1)), drop = FALSE]
     Omega_s <- Omega_s - Omega_s %*% obj$qs1 %*% t(obj$qs1)
   }
-  if (!is.null(obj$qe1)){# project Omega_e perpendicular to qe1
+  if (!is.null(obj$qe1)){# project Omega_e perpendicular to qe1 and project PBce perpendicular to p1
     obj$qe1 <- obj$qe1/vnorm(obj$qe1)
     Omega_e <- newOmega[, length(obj$qs1) + seq.int(1, length.out = length(obj$qe1)), drop = FALSE]
     Omega_e <- Omega_e - Omega_e %*% obj$qe1 %*% t(obj$qe1)
+    obj$PBce <- obj$PBce -  (obj$p1 %*% t(obj$p1)) %*% obj$PBce
   }
   obj$Omega <- cbind(Omega_s, Omega_e)
   return(obj)

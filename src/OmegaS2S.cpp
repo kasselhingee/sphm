912ae25a08106bf64cd62fd9009bc46b537c3ddc
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-01-13 19:29:45 +1100

    unused conversions from R kept updated

--- src/OmegaS2S.cpp
+++ src/OmegaS2S.cpp
@@ -5,8 +5,8 @@ mnlink_Omega_cpp<double> R2OmegaS2S_double(Rcpp::List obj) {
     Eigen::Matrix<double, Eigen::Dynamic, 1> qs1 = Rcpp::as<Eigen::Matrix<double, Eigen::Dynamic, 1>>(obj["qs1"]);
     Eigen::Matrix<double, Eigen::Dynamic, 1> qe1 = Rcpp::as<Eigen::Matrix<double, Eigen::Dynamic, 1>>(obj["qe1"]);
     Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> Omega = Rcpp::as<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>(obj["Omega"]);
-    Eigen::Matrix<double, Eigen::Dynamic, 1> c1 = Rcpp::as<Eigen::Matrix<double, Eigen::Dynamic, 1>>(obj["c1"]);
-    Eigen::Matrix<double, Eigen::Dynamic, 1> PBc = Rcpp::as<Eigen::Matrix<double, Eigen::Dynamic, 1>>(obj["PBc"]);
+    Eigen::Matrix<double, Eigen::Dynamic, 1> ce1 = Rcpp::as<Eigen::Matrix<double, Eigen::Dynamic, 1>>(obj["ce1"]);
+    Eigen::Matrix<double, Eigen::Dynamic, 1> PBce = Rcpp::as<Eigen::Matrix<double, Eigen::Dynamic, 1>>(obj["PBce"]);
     return mnlink_Omega_cpp<double>(p1, qs1, qe1, Omega, c1, PBc);
 }
 
@@ -17,8 +17,8 @@ mnlink_Omega_cpp<a1type> R2OmegaS2S_a1type(Rcpp::List obj) {
     veca1 qs1 = Rcpp::as<veca1>(obj["qs1"]);
     veca1 qe1 = Rcpp::as<veca1>(obj["qe1"]);
     mata1 Omega = Rcpp::as<mata1>(obj["Omega"]);
-    veca1 c1 = Rcpp::as<veca1>(obj["c1"]);
-    veca1 PBc = Rcpp::as<veca1>(obj["PBc"]);
+    veca1 ce1 = Rcpp::as<veca1>(obj["ce1"]);
+    veca1 PBce = Rcpp::as<veca1>(obj["PBce"]);
     return mnlink_Omega_cpp<a1type>(p1, qs1, qe1, Omega, c1, PBc);
 }
 

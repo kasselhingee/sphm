0646eae3fe024c5d6333e7fb6a84005fcb40a2ba
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-01-21 15:43:49 +1100

    better help of mnlink()

--- R/mnlink.R
+++ R/mnlink.R
@@ -1,6 +1,16 @@
 #' Calculate the Mean Given Covariates
+#' @description
 #' Implements mean following Remark 1:
-#' \deqn{\mu(x) = PS^{-1}\left(B_s S(Q_s^\top x_s)  +  \frac{B_e(Q_e[,-1]^\top x_e + c_e[-1])}{Q_e[,1]^\top x_e + c_e[1]}\right)}
+#' \deqn{\mu(x) = P\mathcal{S}^{-1}\left(B_s \mathcal{S}(Q_s^\top x_s)  +  \frac{B_e(Q_e[,-1]^\top x_e + c_e[-1])}{Q_e[,1]^\top x_e + c_e[1]}\right)}
+#' @param xs A matrix of row-vectors of the spherical covariate.
+#' @param xe A matrix of row-vectors of the Euclidean covariates.
+#' @param paramobj Parameters of the mean link. As an object of class "mnlink_Omega" or "mnlink_cann". See [`mnlink_params`]. 
+#' @details
+#' If `paramobj` is of class "mnlink_Omega" then means are computed as
+#' \deqn{\mu(x) = \frac{(1-\|\tilde{y}(x)\|^2) P[,1] + 2 \tilde{y}(x)}{1+\|\tilde{y}(x)\|^2}}
+#' where
+#' \deqn{\tilde{y}(x) = \frac{\Omega_s x_s}{1+Q_s[,1]^\top x_s} + \frac{\Omega_e x_e + \tilde{c}_e}{c_e[1]+{Q_e[,1]}^\top x_e}}
+#' and \eqn{x_s} and \eqn{x_e} are the spherical and Euclidean covariate and \eqn{\tilde{c}_e = P[,-1]B_e c_e[-1]} is the `PBce` element of `paramobj`.
 #' @export
 mnlink <- function(xs = NULL, xe = NULL, paramobj = NULL, check = TRUE){
   if (inherits(paramobj, "mnlink_Omega")){
@@ -8,7 +18,7 @@ mnlink <- function(xs = NULL, xe = NULL, paramobj = NULL, check = TRUE){
     meanlinkS2Scpp(xs, xe, mnlink_Omega_vec(paramobj), length(paramobj$p1))
   }
   if (inherits(paramobj, "mnlink_cann")){
-    # implement here by R
+    mnlink_pred_cann(xs, xe, paramobj)
   }
 }
 

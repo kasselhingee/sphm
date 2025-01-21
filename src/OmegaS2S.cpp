07a18168823797828967c5043de7f093ae8fb549
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-01-21 16:48:50 +1100

    ineqconstraints for Omega now copes with Be and Bs

--- src/OmegaS2S.cpp
+++ src/OmegaS2S.cpp
@@ -11,7 +11,7 @@ veca1 Omega_constraints(veca1 & vec, int p, int qe=0) {
     out(1) = ompar.qs1.squaredNorm() - 1.;
   }
   if (ompar.qe>0){
-    out(1 + (ompar.qs>0)) = ompar.qe1.squaredNorm() - 1.
+    out(1 + (ompar.qs>0)) = ompar.qe1.squaredNorm() - 1.;
   }
   return(out);
 }
@@ -26,13 +26,14 @@ veca1 Omega_constraints_wrap(veca1 & vec, veca1 & ignore1, vecd & dims_in, matd 
 }
 
 //Constraints on the singular values of Omega - not exact unfortunately, just on total sum
-veca1 Omega_ineqconstraints(veca1 & vec, veca1 & ignore1, vecd & p_in, matd & ignore2){
-  int p = int(p_in(0) + 0.1);
+veca1 Omega_ineqconstraints(veca1 & vec, veca1 & ignore1, vecd & dims_in, matd & ignore2){
+  int p = int(dims_in(0) + 0.1);
+  int qe = int(dims_in(1) + 0.1);
   // Convert vector to a mnlink_Omega_cpp object
-  mnlink_Omega_cpp<a1type> ompar = mnlink_Omega_cpp_unvec(vec, p, 0);
+  mnlink_Omega_cpp<a1type> ompar = mnlink_Omega_cpp_unvec(vec, p, qe);
   a1type ssq_sv = (ompar.Omega.transpose() * ompar.Omega).diagonal().sum();
   veca1 out(1);
-  out(0) = ssq_sv - (p-1.);
+  out(0) = ssq_sv - (ompar.qs>0 + ompar.qe>0) * (p-1.);
   return(out);
 }
 

64ae5ddc1d38d7148da94a33e648003b0a56edb5
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-01-29 09:45:37 +1100

    moving internal prelim functions to prelim_*.R files

--- R/prelim.R
+++ R/prelim.R
@@ -14,56 +14,6 @@ prelimobj <- function(y, xs = NULL, xe = NULL, param){
   return(-mean(rowSums(y * predictedmeans)))
 }
 
-
-prelim_ad <- function(y, xs = NULL, xe = NULL, paramobj0, ...){ #paramobj0 is the starting parameter object
-  om0 <- as_mnlink_Omega(paramobj0)
-  # check inputs:
-  mnlink_Omega_check(om0)
-  p <- ncol(y)
-  stopifnot(p == length(om0$p1))
-  if (!is.null(xs)){
-    stopifnot(ncol(xs) == length(om0$qs1))
-  } else {
-    stopifnot(length(om0$qs1) == 0)
-  }
-  if (!is.null(xe)){
-    stopifnot(ncol(xe) == length(om0$qe1))
-  } else {
-    stopifnot(length(om0$qe1) == 0)
-  }
-
-  dims_in <- c(p, length(om0$qe1))
-  obj_tape <- tape_namedfun("prelimobj_cpp", mnlink_Omega_vec(om0), vector(mode = "numeric"), dims_in, cbind(y,xs,xe), check_for_nan = FALSE)
-  constraint_tape <- tape_namedfun("Omega_constraints_wrap", mnlink_Omega_vec(om0), vector(mode = "numeric"), dims_in, matrix(nrow = 0, ncol = 0), check_for_nan = FALSE)
-  ineqconstraint_tape <- tape_namedfun("Omega_constraints_wrap", mnlink_Omega_vec(om0), vector(mode = "numeric"), dims_in, matrix(nrow = 0, ncol = 0), check_for_nan = FALSE)
-
-  # prepare nloptr options
-  default_opts <- list(algorithm = "NLOPT_LD_SLSQP",
-                xtol_rel = 1E-10, #1E-04,
-                tol_constraints_eq = rep(1E-1, 1 + (length(om0$qs1) > 0) + (length(om0$qe1) > 0)),
-                maxeval = 1E4)
-  ellipsis_args <- list(...)
-  combined_opts <- utils::modifyList(default_opts, ellipsis_args)
-  
-  locopt <- nloptr::nloptr(
-    x0 = mnlink_Omega_vec(om0),
-    eval_f = function(theta){obj_tape$eval(theta, vector(mode = "numeric"))},
-    eval_grad_f = function(theta){obj_tape$Jac(theta, vector(mode = "numeric"))},
-    eval_g_eq =  function(theta){constraint_tape$eval(theta, vector(mode = "numeric"))},
-    eval_jac_g_eq =  function(theta){matrix(constraint_tape$Jacobian(theta), byrow = TRUE, ncol = length(theta))},
-    eval_g_ineq =  function(theta){ineqconstraint_tape$eval(theta, vector(mode = "numeric")) - 2},
-    eval_jac_g_ineq =  function(theta){matrix(ineqconstraint_tape$Jacobian(theta), byrow = TRUE, ncol = length(theta))},
-    opts = combined_opts
-  )
-  
-  return(list(
-    solution = Omega_proj(mnlink_Omega_unvec(locopt$solution, p, length(om0$qe1), check = FALSE)),
-    loc_nloptr = locopt
-  ))
-}
-
-
-
 #' Optimisation of the Preliminary Objectivf Function for S2S Link
 #' @details Uses `nloptr`. `NLopt` doesn't have any algorithms for global optimisation with non-linear equality constraints that use provided gradients. So `_parttape` only does local optimisation and uses `NLOPT_LD_SLSQP` which is the only algorithm that takes advantage of derivatives and can handle non-linear equality constraints.
 #' @param paramobj0 is a starting parameter object.

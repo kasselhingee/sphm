df79219a277da8da633e4c0f531c6baf6f07c5c0
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-05-19 14:35:10 +1000

    tests fixed to account for type = 'Shogo' --> fix_qe1 = TRUE

--- tests/testthat/test-prelim.R
+++ tests/testthat/test-prelim.R
@@ -69,7 +69,7 @@ test_that("prelim optimisation works with Sph covars",{
   expect_equal(opt2$solution, omegapar, tolerance = 0.05)
   
   # check global
-  opt3 <- prelim_ad(y, xs = x, paramobj0 = start, type = "Kassel", globalfirst = TRUE)
+  opt3 <- prelim_ad(y, xs = x, paramobj0 = start, globalfirst = TRUE)
   # if (sign(opt3$solution$qe1[1]) != sign(as_mnlink_Omega(paramobj)$qe1[1])){
   #   opt3$solution <- Euc_signswitch(opt3$solution)
   # }
@@ -121,7 +121,7 @@ test_that("prelim optimisation works with Sph+Euc covars", {
   # check global
   # Global seems to get the constraints wrong instantly!!
   # (these are the 3 'h(x)' values, and they should be very close to 0)
-  # opt3 <- prelim_global(y, xs = xs, xe = xe, paramobj0 = start, type = "Kassel")
+  # opt3 <- prelim_global(y, xs = xs, xe = xe, paramobj0 = start)
   # if (sign(opt3$solution$qe1[1]) != sign(as_mnlink_Omega(paramobj)$qe1[1])){
   #   opt3$solution <- Euc_signswitch(opt3$solution)
   # }
@@ -157,7 +157,7 @@ test_that("Shogo with Sph+Euc covars", {
   
   # optimise locally using derivative information
   # starting at the optimum
-  tmp <- prelim_ad(y, xs = xs, xe = xe, paramobj0 = as_mnlink_Omega(paramobj), type = "Shogo")
+  tmp <- prelim_ad(y, xs = xs, xe = xe, paramobj0 = as_mnlink_Omega(paramobj), fix_qe1 = TRUE)
   expect_equal(tmp$solution, as_mnlink_Omega(paramobj), tolerance = 0.05)
   expect_silent(mnlink_Omega_check(tmp$solution))
   expect_silent(mnlink_cann_check(as_mnlink_cann(tmp$solution)))
@@ -174,7 +174,7 @@ test_that("Shogo with Sph+Euc covars", {
   start$Omega <- cbind(start$Omega[,1:qs], 0, start$Omega[,qs + (1:qe)])
   start$qe1 <- c(1, rep(0, qe))
   start$ce1 <- 1
-  opt2 <- prelim_ad(y, xs = xs, xe = xe, paramobj0 = start, type = "Shogo")
+  opt2 <- prelim_ad(y, xs = xs, xe = xe, paramobj0 = start, fix_qe1 = TRUE)
   if (sign(opt2$solution$qe1[1]) != sign(as_mnlink_Omega(paramobj)$qe1[1])){
     opt2$solution <- Euc_signswitch(opt2$solution)
   }

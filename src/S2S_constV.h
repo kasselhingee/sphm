8afcade004c88206e56f5722d198fac58e82fca2
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-05-29 10:46:48 +1000

    deleted functions stubs in S2S_constV.h that were obsolete

--- src/S2S_constV.h
+++ src/S2S_constV.h
@@ -14,23 +14,9 @@ veca1 ull_S2S_constV(mata1 y, mata1 xs, mata1 xe, mnlink_Omega_cpp<a1type> om, a
 // [[Rcpp::export]]
 veca1 ull_S2S_constV_forR(mata1 y, mata1 xs, mata1 xe, veca1 omvec, a1type k, a1type a1, veca1 aremaining, mata1 Kstar);
 
-//tape for mean wrt mean with the base point P[,1] fixed for calculating the axes. Returned tape should have dynamic parameters of: k, a1, aremaining, as.vector(Gstar).
-pADFun tape_ull_S2S_constV_mean(veca1 & omvec, a1type k, a1type a1, veca1 aremaining, mata1 Gstar, vecd & p_in, matd & yx);
-
-// tape for axes
 // axes parameterised by the Cayley transform of K* in H*(P[,1])K*
 // the Cayley transform will work okay so long as K* is reasonably close to the basis given by H*. If P[,1] is reasonably close to north pole (due to standardisation of the data) and the axes reasonably close to the ambient basis axes, then K* will be reasonably close to the identity. This is good for using Cayley transforms.
-//returned tape should have as dynamic parameters: P1, as.vector(om), k, a1, aremaining
-pADFun tape_ull_S2S_constV_axes(veca1 & vecCayaxes, mnlink_Omega_cpp<a1type> om, a1type k, a1type a1, veca1 aremaining, vecd & p_in, matd & yx);
 
-// tape for aremaining. Like _mean but everything else constant
-pADFun tape_ull_S2S_constV_aremaining(veca1 & aremaining, mnlink_Omega_cpp<a1type> om, a1type k, a1type a1, mata1 Gstar, vecd & p_in, matd & yx);
-
-
-// tape for k
-pADFun tape_ull_S2S_constV_k(veca1 k, mnlink_Omega_cpp<a1type> om, a1type a1, veca1 & aremaining, mata1 Gstar, vecd & p_in, matd & yx);
-
-// experimental: tape for everything all together except a1
 // make the independent values omvec, k, aremaining and vecCayaxes,
 // [[Rcpp::export]]
 pADFun tape_ull_S2S_constV_nota1(veca1 omvec, a1type k, a1type a1, veca1 aremaining, mata1 Kstar, vecd & p_in, vecd & qe_in, matd & yx);

6b5ed3d0532d59261bf394400422c3d318897cd6
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-09-04 08:53:45 +1000

    removed alignedG C++ code

--- src/function_map.h
+++ src/function_map.h
@@ -4,16 +4,12 @@
 #include <sphm_forward.h>
 #include "prelimS2S.h"
 #include "Omega.h"
-#include "S2S_alignedG.h"
 
 // defining a function map that will convert string names to a pointer to the function
 std::map<std::string, generalfunction> function_map = {
    {"prelimobj_cpp", &prelimobj_cpp},
    {"Omega_constraints_wrap", &Omega_constraints_wrap},
-   {"Omega_ineqconstraints", &Omega_ineqconstraints},
-   {"ull_S2S_alignedG_mean", &ull_S2S_alignedG_mean},
-   {"ull_S2S_alignedG_a", &ull_S2S_alignedG_a},
-   {"ull_S2S_alignedG_k", &ull_S2S_alignedG_k},
+   {"Omega_ineqconstraints", &Omega_ineqconstraints}
 };
 
 
